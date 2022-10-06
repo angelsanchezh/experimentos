40fa314c6c2f9508de6df2d3ffac9053fda98586
angelsanchezh <angelsanchezh@gmail.com>
2022-10-06 17:49:02 -0500

    slider2

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -3,6 +3,15 @@ import './SliderStyles.css';
 import Drcinco from '../../Assets/Drcinco.jpg';
 import Drcuatro from '../../Assets/Drcuatro.jpg';
 import Drseis from '../../Assets/Drseis.jpg';
+import Druno from '../../Assets/Druno.jpg';
+import Drdos from '../../Assets/Drdos.jpg';
+import Drtres from '../../Assets/Drtres.jpg';
+import Drsiete from '../../Assets/Drsiete.jpg';
+import Drocho from '../../Assets/Drocho.jpg';
+import Drnueve from '../../Assets/Drnueve.jpg';
+
+
+
 
 const Slider = () => {
   return (
@@ -20,59 +29,91 @@ const Slider = () => {
 
     <div className="carousel-item active" data-bs-interval="10000">
       
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="row row-cols-1 row-cols-md-4 g-3">
        
        <div className="col">
        <div className="card h-100">
       <img src={Drcinco} className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 className="card-title">Dra Maria Castillo</h5>
+        <p className="card-text">
+        Neurologia
+        <br />Reumatologia
+        <br /> Fisioterapia 
+
+        </p>
       </div>
       <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+        <small className="text-muted">Mar. a Jue. 9am a 5pm</small>
+        <button type="button" class="btn btn-dark">Agendar</button>
       </div>
     </div>
        </div>
   
-       <div class="col">
-    <div class="card h-100">
+       <div className="col">
+    <div className="card h-100">
       <img src={Drcuatro} className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <h5 className="card-title">Dra Martha Pereira</h5>
+        <p className="card-text">Radiologia
+        <br />Traumatologia
+        <br /> Reumatologia 
+
+        </p>
       </div>
       <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+        <small className="text-muted">Lun. a Vie. 9am a 5pm</small>
+        <button type="button" class="btn btn-dark">Agendar</button>
       </div>
     </div>
        </div>
 
 
-       <div class="col">
-    <div class="card h-100">
+       <div className="col">
+    <div className="card h-100">
       <img src={Drseis} class="card-img-top" alt="..."/>
-      <div class="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <div className="card-body">
+        <h5 className="card-title">Dra. Mariela Melendez</h5>
+        <p className="card-text">Neurologia
+        <br />Traumatologia
+        <br /> Reumatologia 
+        </p>
       </div>
       <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
+        <small className="text-muted">Lun. a Vie. 9am a 5pm</small>
+        <button type="button" class="btn btn-dark">Agendar</button>
       </div>
     </div>
        </div>
 
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drnueve} class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Lic. Jenny Paredes</h5>
+        <p className="card-text">
+        Fisioterapia
+        <br />Traumatologia
+        <br /> Deportiva  
+        </p>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">Mar. a Jue. 9am a 5pm</small>
+        <button type="button" class="btn btn-dark">Agendar</button>
+      </div>
+    </div>
+       </div>
 
       </div>
 
       </div>
 
       <div className="carousel-item" data-bs-interval="2000">
-      <div className="row row-cols-1 row-cols-md-3 g-4">
+      <div className="row row-cols-1 row-cols-md-4 g-3">
        
        <div className="col">
        <div className="card h-100">
-      <img src={Drcinco} className="card-img-top" alt="..."/>
+      <img src= {Druno} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -83,9 +124,9 @@ const Slider = () => {
     </div>
        </div>
   
-       <div class="col">
-    <div class="card h-100">
-      <img src={Drcuatro} className="card-img-top" alt="..."/>
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drdos} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -97,10 +138,10 @@ const Slider = () => {
        </div>
 
 
-       <div class="col">
-    <div class="card h-100">
+       <div className="col">
+    <div className="card h-100">
       <img src={Drseis} class="card-img-top" alt="..."/>
-      <div class="card-body">
+      <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       </div>
@@ -110,17 +151,29 @@ const Slider = () => {
     </div>
        </div>
 
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drnueve} class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+       </div>
 
       </div>
 
       </div>
 
     <div className="carousel-item">
-    <div className="row row-cols-1 row-cols-md-3 g-4">
+    <div className="row row-cols-1 row-cols-md-4 g-3">
        
        <div className="col">
        <div className="card h-100">
-      <img src={Drcinco} className="card-img-top" alt="..."/>
+      <img src={Drsiete} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -131,9 +184,9 @@ const Slider = () => {
     </div>
        </div>
   
-       <div class="col">
-    <div class="card h-100">
-      <img src={Drcuatro} className="card-img-top" alt="..."/>
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drocho} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -145,10 +198,10 @@ const Slider = () => {
        </div>
 
 
-       <div class="col">
-    <div class="card h-100">
-      <img src={Drseis} class="card-img-top" alt="..."/>
-      <div class="card-body">
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drnueve} class="card-img-top" alt="..."/>
+      <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
       </div>
@@ -158,6 +211,18 @@ const Slider = () => {
     </div>
        </div>
 
+       <div className="col">
+    <div className="card h-100">
+      <img src={Drnueve} class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+       </div>
 
       </div>
 
